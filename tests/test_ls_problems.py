4e42c96761439138baba93cee57a27aae0b6db72
ReshmaRegijc <reshmaregi.jc@gmail.com>
2021-09-18 08:41:29 +0530

    Added addBinary

--- tests/test_ls_problems.py
+++ tests/test_ls_problems.py
@@ -1,4 +1,5 @@
 from src.lc_problems import add1,add2
+from src.addbinary import addBinary
 
 def test_add1():
     assert add1(7)==8
@@ -11,3 +12,9 @@ def test_add2():
 
 def test_add2_fail():
     assert add2(6)==8
+
+def test_addBinary():
+    assert addBinary("11","1") == "100"
+
+def test_addBinary1():
+    assert addBinary("1010","1011") == "10101"
